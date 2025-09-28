42dd57578bc2ba74b85b19de6ec6e724bcae6c9a
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 23:27:22 +0530

    shipping file created

---
+++ shipping.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# -------------------------
+# Color Codes
+# -------------------------
+R="\e[31m" #Red
+G="\e[32m" #Green
+Y="\e[33m" #Yellow
+N="\e[0m"  #No Color    
+
+#check if current user has root access
+user_rootaccess=$(id -u)
+
+mongodb_host="mongodb.techup.fun"
+script_dir=$(pwd)
+#create folder in log folder 
+logs_folder="/var/log/shell-roboshop"
+#removing .sh from script name to create a log file
+script_name=$( echo $0 | cut -d "." -f1 )
+log_file="$logs_folder/$script_name.log" 
+
+mkdir -p $logs_folder
+echo "script started excuting at : $(date)" | tee -a $log_file
+#if user does not have root access
+if [ $user_rootaccess -ne 0 ]; then 
+    echo "Error:: Please run the script using root access"
+    exit 1
+fi
+
+# Function to validate installation status
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "error:: $2 $R failed $N" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 $G Completed $N" | tee -a $log_file
+    fi
+
+}
+
+dnf install maven -y &>>$log_file
+validate $? "Maven installation"
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$log_file
+validate $? "roboshop user" 
+
+mkdir /app &>>$log_file
+validate $? "/app directory creation"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$log_file
+validate $? "shipping download"
+
+cd /app 
+unzip /tmp/shipping.zip &>>$log_file
+validate $? "shipping unzip"
+
+cd /app 
+mvn clean package &>>$log_file
+validate $? "maven build"
+
+mv target/shipping-1.0.jar shipping.jar &>>$log_file
+validate $? "shipping rename"
+
+cp $script_dir/shipping.service /etc/systemd/system/shipping.service &>>$log_file
+validate $? "shipping service file copy"
+
+
+systemctl daemon-reload &>>$log_file
+validate $? "daemon reload"
+systemctl enable shipping &>>$log_file
+validate $? "shipping enable"
+systemctl start shipping &>>$log_file
+validate $? "shipping start"
+
+dnf install mysql -y &>>$log_file
+validate $? "mysql client"
+
+mysql -h mysql.techup.fun -uroot -pRoboShop@1 < /app/db/schema.sql &>>$log_file
+validate $? "shipping schema load"
+
+mysql -h mysql.techup.fun -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$log_file
+validate $? "shipping master data load" 
+
+systemctl restart shipping &>>$log_file
+validate $? "shipping restart"
+
+echo -e "shipping setup completed $G Successful $N" | tee -a $log_file
