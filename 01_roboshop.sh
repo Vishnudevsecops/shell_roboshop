286bb5bd8dd8ec47a5b5f8cf06487e9329e01b39
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 16:20:07 +0530

    rename file name

--- 01_roboshop.sh
+++ 01_roboshop.sh
@@ -5,7 +5,8 @@ sg_id="sg-0473e0dab07b57594"
 
 for instance in $@
 do 
-    instace_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceID' --output text)
+    instace_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro 
+    --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceID' --output text)
 
     if [ $? != frontend ]; then
         ip_address=$(aws ec2 describe-instances --instance-ids $instace_id --query 'Reservations[0].Instances[0].PrivateIPAddress' --output text)
