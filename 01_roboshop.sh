471e9dc78ba746e2855e910350beb97ec57501f2
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 17:15:43 +0530

    rename file name

--- 01_roboshop.sh
+++ 01_roboshop.sh
@@ -2,6 +2,8 @@
 
 ami_id="ami-09c813fb71547fc4f"
 sg_id="sg-0473e0dab07b57594"
+zone_id="Z01937573HQBFLFNZX4SW"
+domain_name="techup.fun"
 
 for instance in $@
 do 
@@ -13,20 +15,39 @@ do
 
     echo "Waiting for instance $instance_id to be in 'running' state..."
     aws ec2 wait instance-running --instance-ids "$instance_id"
-    
+
     if [ $instance != "frontend" ]; then
         ip_address=$(aws ec2 describe-instances \
         --instance-ids $instance_id \
         --query 'Reservations[0].Instances[0].PrivateIpAddress' \
         --output text)
+        record_name="$instance.$domain_name"
     else
         ip_address=$(aws ec2 describe-instances \
         --instance-ids $instance_id \
         --query 'Reservations[0].Instances[0].PublicIpAddress' \
         --output text)
+        record_name="$domain_name"
     fi
 
-    echo "Instance ID: $instance_id, IP Address: $ip_address"
+    echo "$instance: $ip_address"
 
-    
+    aws route53 change-resource-record-sets \
+        --hosted-zone-id $zone_id \
+        --change-batch '
+        {
+            "Comment": "Updating record set"
+            ,"Changes": [{
+                "Action"              : "UPSERT"
+                ,"ResourceRecordSet"  : {
+                "Name"              : "'$record_name'"
+                ,"Type"             : "A"
+                ,"TTL"              : 1
+                ,"ResourceRecords"  : [{
+                "Value"         : "'$ip_address'"
+            }]
+        }
+        }]
+  }
+  '
 done
