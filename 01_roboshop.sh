748133126dcef7d175119e0219bd2e5fa93d0148
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 16:40:39 +0530

    rename file name

--- 01_roboshop.sh
+++ 01_roboshop.sh
@@ -5,16 +5,28 @@ sg_id="sg-0473e0dab07b57594"
 
 for instance in $@
 do 
-    instace_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro 
-    --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceID' --output text)
+    instance_id=$(aws ec2 run-instances \
+    --image-id $ami_id --instance-type t3.micro \
+    --security-group-ids $sg_id \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' --output text)
 
-    if [ $? != frontend ]; then
-        ip_address=$(aws ec2 describe-instances --instance-ids $instace_id --query 'Reservations[0].Instances[0].PrivateIPAddress' --output text)
+    echo "Waiting for instance $instance_id to be in 'running' state..."
+    aws ec2 wait instance-running --instance-ids "$instance_id"
+    
+    if [ $instance != "frontend" ]; then
+        ip_address=$(aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+        --output text)
     else
-        ip_address=$(aws ec2 describe-instances --instance-ids $instace_id --query 'Reservations[0].Instances[0].PublicIPAddress' --output text)
+        ip_address=$(aws ec2 describe-instances \
+        --instance-ids $instance_id \
+        --query 'Reservations[0].Instances[0].PublicIpAddress' \
+        --output text)
     fi
 
-    echo "Instance ID: $instace_id, IP Address: $ip_address"
+    echo "Instance ID: $instance_id, IP Address: $ip_address"
 
     
 done
