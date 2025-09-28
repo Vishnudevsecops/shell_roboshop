67c19713f92bcc37a93ba573525de13d37eb15b1
rnnvishnu <satish.ramayanam@gmail.com>
2025-09-28 17:26:43 +0530

    rename file name

---
+++ mongodb.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# -------------------------
+# Color Codes
+# -------------------------
+R="\e[31m" #Red
+G="\e[32m" #Green
+Y="\e[33m" #Yellow
+N="\e[0m"  #No Color    
+
+#check if current user has root access
+user_rootaccess=$(id -u)
+
+#create folder in log folder 
+logs_folder="/var/log/shell-roboshop"
+#removing .sh from script name to create a log file
+script_name=$( echo $0 | cut -d "." -f1 )
+log_file="$logs_folder/$script_name.log" 
+
+mkdir -p $logs_folder
+echo "script started excuting at : $(date)" | tee -a $log_file
+#if user does not have root access
+if [ $user_rootaccess -ne 0 ]; then 
+    echo "Error:: Please run the script using root access"
+    exit 1
+fi
+
+# Function to validate installation status
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "error:: $2 $R failed $N" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 $G Completed $N" | tee -a $log_file
+    fi
+
+}
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+validate $? "Mongodb repo setup"
+
+dnf install mongodb-org -y &>>$log_file
+validate $? "Mongodb"
+
+systemctl enable mongod  &>>$log_file
+validate $? "Mongodb enable"
+systemctl start mongod &>>$log_file
+validate $? "Mongodb start"
